d8d5db72f38d2726f81b649e836eb6cc14d7ace7
Jack Page <jdpboc98@gmail.com>
2020-04-28 16:41:41 +0100

    Autocomplete supports context-switching double keypresses ~ Shortcuts to switch context by double typing an object type are now supported again after they were broken by the introduction of automatic autocompletion generation

--- pylux/lib/autocomplete.py
+++ pylux/lib/autocomplete.py
@@ -1,3 +1,6 @@
+from pylux.lib.constant import CONTEXT_KEYS
+
+
 class Keymapper:
 
     def __init__(self, interpreter, config):
@@ -15,13 +18,23 @@ class Keymapper:
         relevant_keys = []
         n = len(fragment.split())
         expected_input = self.interpreter.get_expected_input(fragment)
-        if not expected_input:
-            return None
-        for exp in expected_input:
-            if exp in self.keys:
-                relevant_keys.append((exp, self.keys[exp]))
+        if expected_input:
+            for exp in expected_input:
+                if exp in self.keys:
+                    relevant_keys.append((exp, self.keys[exp]))
+
+        # Add on extra commands which are not covered by the interpreter
+        # Double-type an object type to switch context to that
+        if n == 1:
+            k1 = fragment.split()[0]
+            if k1 in CONTEXT_KEYS:
+                relevant_keys.append((k1, self.keys[k1]))
+
         # We need to add on a pre-space to the keyword if it is appearing after the
-        # refs field (i.e. if the current command length is 2)
+        # refs field (i.e. if the current command length is 2). Also just return None
+        # if there are no relevant keys.
+        if not relevant_keys:
+            return None
         if n == 0 or n == 1:
             return self._generate_keymap(relevant_keys)
         elif n == 2:
--- pylux/lib/constant.py
+++ pylux/lib/constant.py
@@ -9,3 +9,6 @@ FIXTURE_TYPE = ('fixture', 'Fixture', {})
 GROUP_TYPE = ('group', 'Group', {'fixtures': []})
 FILTER_TYPE = ('filter', 'Filter', {'k': '', 'v': ''})
 REGISTRY_TYPE = ('registry', 'Registry', {'table': {}})
+
+
+CONTEXT_KEYS = [globals()[i][1] for i in globals().keys() if '_TYPE' in i]
